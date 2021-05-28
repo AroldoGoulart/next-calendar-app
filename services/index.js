ea24c1aeb5439e9d2bcfba84ee7c708d4f9750fb
= <aroldogooulart@gmail.com>
2021-05-28 14:11:22 -0400

    fix: api change baseurl

--- services/index.js
+++ services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:1337/api/',
+  baseURL: 'http://195.238.80.190:5353/api/',
   headers: {
     // Overwrite Axios's automatically set Content-Type
     'Content-Type': 'application/json'
